de95d9ce681ffbb3b7b9c20a484546291eb25c6f
Dominik Zduńczyk <dominik.zdunczyk@gmail.com>
2022-02-14 12:35:20 +0100

    Bugfix/fix dynos run (#4)
    * extend run options for yarn start
    
    * Squashed commit of the following:
    
    commit 214a9c8f2fc0e8ed2b3003873f0638b8082ad946
    Author: Bartosz Kostrowiecki <bartosz.kostrowiecki@gmail.com>
    Date:   Sat Feb 12 17:25:21 2022 +0100
    
        fix admin compilation
    
    * package json conflicts solve
    
    * resolve merge conflict
    
    * delete conflicts

--- src/NestPress/NestPress/admin-ui/src/src/extensions/26436d7a25a948b71ae5e01b94b67bb7ac7bef8eca9080777d01d61f8cc5742d/post-permissions.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'greeter',
-  template: `<h1>{{ greeting }}</h1>`,
-})
-export class GreeterComponent {
-  greeting = 'Hello!';
-}
